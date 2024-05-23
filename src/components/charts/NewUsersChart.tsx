6831f932c818f4c104b8b7aaf7ebbed73fe3a6d0
danilo2504 <danilobautista2004@gmail.com>
2024-05-23 17:13:00 +0200

    change isLoading attribute for isPending

--- src/components/charts/NewUsersChart.tsx
+++ src/components/charts/NewUsersChart.tsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import type { ChartData, ChartOptions } from 'chart.js';
-import { Chart as ChartJS, registerables } from 'chart.js/auto';
-import { Bar} from 'react-chartjs-2';
-import TimeRangeSelector from '../common/TimeRangeSelector';
-import { useQuery } from '@tanstack/react-query';
-import { getNewUsersStats } from '@/src/services/users';
+import React, { useState, useEffect } from "react";
+import type { ChartData, ChartOptions } from "chart.js";
+import { Chart as ChartJS, registerables } from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+import TimeRangeSelector from "../common/TimeRangeSelector";
+import { useQuery } from "@tanstack/react-query";
+import { getNewUsersStats } from "@/src/services/users";
 
-type BarOptionsProps = ChartOptions<'bar'>;
-type BarDataProps = ChartData<'bar'>;
+type BarOptionsProps = ChartOptions<"bar">;
+type BarDataProps = ChartData<"bar">;
 
 type ChartDayProps = {
   length: 7;
@@ -21,72 +21,99 @@ type ChartMonthProps = {
 
 const generateDailyReport = (values: ChartDayProps | []): BarDataProps => {
   return {
-    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-    datasets: [{
-      label: 'Daily New Users',
-      data: values,
-      backgroundColor: 'rgba(255, 159, 64, 0.7)',
-      borderSkipped: true
-    }]
+    labels: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+    datasets: [
+      {
+        label: "Daily New Users",
+        data: values,
+        backgroundColor: "rgba(255, 159, 64, 0.7)",
+        borderSkipped: true,
+      },
+    ],
   };
 };
 const generateWeeklyReport = (values: ChartWeekProps | []): BarDataProps => {
   return {
-    labels: ['1º week', '2º week', '3º week', '4º week', '5º week'],
-    datasets: [{
-      label: 'Weekly New Users',
-      data: values,
-      backgroundColor: 'rgba(54, 162, 235, 0.7)',
-      borderWidth: 0
-    }]
+    labels: ["1º week", "2º week", "3º week", "4º week", "5º week"],
+    datasets: [
+      {
+        label: "Weekly New Users",
+        data: values,
+        backgroundColor: "rgba(54, 162, 235, 0.7)",
+        borderWidth: 0,
+      },
+    ],
   };
 };
 const generateMonthlyReport = (values: ChartMonthProps | []): BarDataProps => {
   return {
-    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-    datasets: [{
-      label: 'Monthly New Users',
-      data: values,
-      backgroundColor: 'rgba(75, 192, 192, 0.7)',
-      borderWidth: 0
-    }]
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Monthly New Users",
+        data: values,
+        backgroundColor: "rgba(75, 192, 192, 0.7)",
+        borderWidth: 0,
+      },
+    ],
   };
 };
 
 interface DataProps {
-   title: string; 
-   report: BarDataProps
+  title: string;
+  report: BarDataProps;
 }
 
 const NewUsersChart = () => {
-  const intervals = ['Day', 'Week', 'Month'];
-  const [interval, setInterval] = useState<string>('day');
-  const { data, isLoading, refetch, isRefetching } = useQuery({ 
-    queryKey: ['new_users_stats', interval], 
-    queryFn: () => getNewUsersStats(interval)
+  const intervals = ["Day", "Week", "Month"];
+  const [interval, setInterval] = useState<string>("day");
+  const { data, isPending, refetch, isRefetching } = useQuery({
+    queryKey: ["new_users_stats", interval],
+    queryFn: () => getNewUsersStats(interval),
   });
 
   // Use react-query to manage loading states when loading and refetchs
   const chartData: Record<string, DataProps> = {
     day: {
-      title: isLoading ? 'Loading...' : 'Daily New Users',
-      report: isRefetching 
-      ? generateDailyReport([]) 
-      : generateDailyReport(data),
+      title: isPending ? "Loading..." : "Daily New Users",
+      report: isRefetching
+        ? generateDailyReport([])
+        : generateDailyReport(data),
     },
     week: {
-      title: isLoading ? 'Loading...' : 'Weekly New Users',
-      report: isRefetching 
-      ? generateWeeklyReport([])
-      : generateWeeklyReport(data)
+      title: isPending ? "Loading..." : "Weekly New Users",
+      report: isRefetching
+        ? generateWeeklyReport([])
+        : generateWeeklyReport(data),
     },
     month: {
-      title: isLoading ? 'Loading...' : 'Monthly New Users',
-      report: isRefetching 
-      ? generateMonthlyReport([])
-      : generateMonthlyReport(data)
-    }
-  }
+      title: isPending ? "Loading..." : "Monthly New Users",
+      report: isRefetching
+        ? generateMonthlyReport([])
+        : generateMonthlyReport(data),
+    },
+  };
   const chartOptions: BarOptionsProps = {
     scales: {
       y: {
@@ -94,31 +121,31 @@ const NewUsersChart = () => {
         suggestedMin: 30,
         suggestedMax: 50,
         grid: {
-          tickColor: 'transparent',
+          tickColor: "transparent",
         },
         ticks: {
-          color: 'gray'
-        }
+          color: "gray",
+        },
       },
       x: {
         grid: {
-          color: 'transparent',
-          tickColor: 'transparent',
+          color: "transparent",
+          tickColor: "transparent",
         },
         ticks: {
-          color: 'gray'
-        }
-      }
+          color: "gray",
+        },
+      },
     },
     plugins: {
       legend: {
-        display: false
+        display: false,
       },
       title: {
         display: true,
-        text: chartData[interval].title
-      }
-    }
+        text: chartData[interval].title,
+      },
+    },
   };
 
   useEffect(() => {
@@ -126,26 +153,21 @@ const NewUsersChart = () => {
   }, []);
   useEffect(() => {
     refetch();
-  }, [interval])
+  }, [interval]);
 
   return (
-    <div className='card-base h-min w-full'>
-      <div className='flex justify-between items-center mb-4'>
-        <h2 className='text-[#344767] font-semibold py-2'>
-          Data Users
-        </h2>
+    <div className="card-base h-min w-full">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-[#344767] font-semibold py-2">Data Users</h2>
         <TimeRangeSelector
-          onChange={setInterval} 
+          onChange={setInterval}
           intervals={intervals}
           currentInterval={interval}
         />
       </div>
-      <Bar
-        data={chartData[interval].report}
-        options={chartOptions}
-      />
+      <Bar data={chartData[interval].report} options={chartOptions} />
     </div>
-  )
+  );
 };
 
 export default NewUsersChart;
--- src/components/tables/UsersByCountry.tsx
+++ src/components/tables/UsersByCountry.tsx
@@ -1,58 +1,53 @@
-import { useQuery, UseQueryResult } from '@tanstack/react-query';
-import { getUsersByCountry } from '@/src/services/users';
-import UserByCountrySkeleton from './UsersByCountrySkeleton';
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { getUsersByCountry } from "@/src/services/users";
+import UserByCountrySkeleton from "./UsersByCountrySkeleton";
 
 interface UsersByCountry {
-    _count: number,
-    country: string,
-    position: number
+  _count: number;
+  country: string;
+  position: number;
 }
 
 const UsersByCountryTable = () => {
-  const columns = ['Top', 'Country', 'Users'];
-  const { data, isLoading }: UseQueryResult<UsersByCountry[]> = useQuery({ 
-    queryKey: ['ranking_by_country'], 
-    queryFn: getUsersByCountry 
+  const columns = ["Top", "Country", "Users"];
+  const { data, isPending }: UseQueryResult<UsersByCountry[]> = useQuery({
+    queryKey: ["ranking_by_country"],
+    queryFn: getUsersByCountry,
   });
 
-  return(
-    <div className='card-base px-4 h-min sticky top-4'>
-      <h2 className='text-[#344767] font-semibold py-2 mb-4'>
-        Ranking
-      </h2>
+  return (
+    <div className="card-base px-4 h-min sticky top-4">
+      <h2 className="text-[#344767] font-semibold py-2 mb-4">Ranking</h2>
 
-      <table className='w-full text-sm text-left text-gray-500'>
-        <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
             {columns.map((label, i) => (
-              <th scope='col' className='p-4' key={label + i}>
+              <th scope="col" className="p-4" key={label + i}>
                 {label}
               </th>
             ))}
           </tr>
         </thead>
 
-        <UserByCountrySkeleton length={5} isVisible={isLoading}/>
-        {!isLoading && 
+        <UserByCountrySkeleton length={5} isVisible={isPending} />
+        {!isPending && (
           <tbody>
             {data?.map((row: UsersByCountry) => (
-              <tr key={row.country} className='bg-white border-b hover:bg-gray-50'>
-                <td className='p-4'>
-                  {row.position}
-                </td>
-                <td className='p-4'>
-                  {row.country}
-                </td>
-                <td className='p-4'>
-                  {row._count}
-                </td>
+              <tr
+                key={row.country}
+                className="bg-white border-b hover:bg-gray-50"
+              >
+                <td className="p-4">{row.position}</td>
+                <td className="p-4">{row.country}</td>
+                <td className="p-4">{row._count}</td>
               </tr>
             ))}
           </tbody>
-        }
+        )}
       </table>
     </div>
-  )
+  );
 };
 
 export default UsersByCountryTable;
