46e68e8effc1fb20fa7438ae9117cb5b8a329c57
danilo2504 <danilobautista2004@gmail.com>
2024-05-23 21:53:17 +0200

    add fix axios

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { signOut } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
+  // baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
+  baseURL: "https://admin-api-copy-production.up.railway.app/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
